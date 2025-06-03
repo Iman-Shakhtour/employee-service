dce3411590fe3c406d4cdf8928a918baecbdd6e6
Iman-Shakhtour <202201318@bethlehem.edu>
2025-06-03 12:54:44 +0300

    refactor: Comment out SecurityConfig class and its methods for future reference

--- employee_service/src/main/java/com/lab/employee_service/SecurityConfig.java
+++ employee_service/src/main/java/com/lab/employee_service/SecurityConfig.java
@@ -1,20 +1,20 @@
-package com.lab.employee_service;
+// package com.lab.employee_service;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-@Configuration
-public class SecurityConfig {
+// @Configuration
+// public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())         // تعطيل CSRF
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()         // السماح لكل الطلبات
-            );
-        return http.build();
-    }
-}
+//     @Bean
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//         http
+//             .csrf(csrf -> csrf.disable())         // تعطيل CSRF
+//             .authorizeHttpRequests(auth -> auth
+//                 .anyRequest().permitAll()         // السماح لكل الطلبات
+//             );
+//         return http.build();
+//     }
+// }
